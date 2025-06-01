d8ce565ff2ef43d5e1258aa0c9b279d7542e759b
DatNDT <trinhdat0123@gmail.com>
2025-06-01 22:41:06 +0700

    fix env

--- src/react-native-config.d.ts
+++ src/react-native-config.d.ts
@@ -3,6 +3,9 @@ declare module 'react-native-config' {
     API_URL_AUTH: string;
     API_URL_WALLET: string;
     API_URL_NETWORK: string;
+    API_URL_TOKEN: string;
+    API_URL_UPLOAD: string;
+    API_URL_POST: string;
     API_URL_TRANSACTION: string;
     API_URL_NOTIFICATION: string;
   }
